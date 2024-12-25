314360e7789adbd81dae28c20b2c72d6a6926519
RabieImghi <rabieaitimghi7@gmail.com>
2024-12-25 19:34:32 +0100

    create project spring Boot for partie back end

---
+++ YouCafeteria/src/main/java/org/rabie/youcafeteria/YouCafeteriaApplication.java
@@ -0,0 +1,13 @@
+package org.rabie.youcafeteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouCafeteriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouCafeteriaApplication.class, args);
+    }
+
+}
---
+++ YouCafeteria/src/test/java/org/rabie/youcafeteria/YouCafeteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rabie.youcafeteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouCafeteriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
