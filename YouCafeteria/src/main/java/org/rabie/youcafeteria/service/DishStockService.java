0b1e38fe51db952a6633cc59664949b9343000d6
RabieImghi <rabieaitimghi7@gmail.com>
2025-01-18 13:49:57 +0100

    manage dish

--- YouCafeteria/src/main/java/org/rabie/youcafeteria/service/DishStockService.java
+++ YouCafeteria/src/main/java/org/rabie/youcafeteria/service/DishStockService.java
@@ -1,7 +1,14 @@
 package org.rabie.youcafeteria.service;
 
+import org.rabie.youcafeteria.domain.DishStock;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface DishStockService {
+    DishStock save(DishStock dishStock);
+    DishStock update(DishStock dishStock);
+    void delete(Long id);
+    DishStock findById(Long id);
+    Page<DishStock> findAll(int page, int size);
 }
